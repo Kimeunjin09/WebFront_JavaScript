c4fc9c6033dd26c90830a203b36fd262159126c0
Kimenjin09 <rladmswls85@gmail.com>
2023-02-20 10:04:49 +0900

    no message

---
+++ js/01_개요.js
@@ -0,0 +1,16 @@
+// 한줄 주석
+/* 범위 주석 */
+
+// css가 <style> 태그 내부라면 js는 <script> 태그 내부라고 생각하기
+
+function btnClick2() {
+    alert("external1 버튼이 클릭됨")
+}
+
+function changeColor1() {
+    document.getElementById("box").style.backgroundColor = "red";
+}
+
+function changeColor2() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+}
